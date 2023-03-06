c8fadd5dc9804d517a5c2d17ba9ea2a13e974951
Macdaddy4sure <Macdaddy4sure@gmail.com>
2023-03-06 13:14:43 -0800

    AI Server Manager Upload

---
+++ AIServerManager.cpp
@@ -0,0 +1,27 @@
+/*
+    Copyright(c) 2021 Tyler Crockett | Macdaddy4sure.com
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http ://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissionsand
+    limitations under the License.
+*/
+
+#include "AIServerManager.h"
+
+/*
+    Purpose: Create an application for running on virtual hosts that solves the following problems.
+    1. Receive and send image files from the client device for long term memory.
+        a. Client device can be: Windows OS, Linux, and Android
+    2. Receive input from the client for CUDA processing.
+    3. Query public MySQL database
+    4. 
+*/
+
